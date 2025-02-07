9b4e89e21ce3e8d73546fc83972cb62fc3f161b9
Stevexchia <stevexchia@gmail.com>
2025-02-07 11:31:16 +0800

    Level 2: Add tasks to list

--- src/main/java/Astra.java
+++ src/main/java/Astra.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Astra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>(); // List to store tasks
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Astra!");
@@ -17,11 +19,22 @@ public class Astra {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                if (tasks.isEmpty()) {
+                    System.out.println(" No tasks added yet.");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks.add(userInput); // Store the task
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + userInput);
+                System.out.println("____________________________________________________________");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + userInput); // Echo user input
-            System.out.println("____________________________________________________________");
         }
 
         scanner.close(); // Close the scanner
