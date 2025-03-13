a7571a0d0fa10052ee675968eeb7ce32612458fd
Stevexchia <stevexchia@gmail.com>
2025-03-14 01:28:25 +0800

    Feature: Implement date & time formatting for deadline & event

---
+++ src/main/java/piggy/commands/FindByDateCommand.java
@@ -0,0 +1,33 @@
+package piggy.commands;
+
+import piggy.data.TaskList;
+import piggy.ui.Ui;
+import piggy.data.Storage;
+import piggy.exceptions.PiggyException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class FindByDateCommand extends Command {
+    private final LocalDate date;
+
+    public FindByDateCommand(String arguments) throws PiggyException {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            this.date = LocalDate.parse(arguments.trim(), formatter);
+        } catch (DateTimeParseException e) {
+            throw new PiggyException("Oink! Invalid date format! Use: findbydate <yyyy-MM-dd>");
+        }
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws PiggyException {
+        tasks.filterTasksByDate(date);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
